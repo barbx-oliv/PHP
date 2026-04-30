f73d66a2045f9b1c21f7deec9eed5f0696e555d6
Bárbara de Oliveira Cruz <barbara.o.cruz@aluno.senai.br>
2026-04-30 10:09:19 -0300

    README e inicio S6

--- aula07/index.php
+++ aula07/index.php
@@ -6,30 +6,37 @@
     <title>Cargos_Funcionários</title>
     <style>
         body {
-            background-color: gray;
+            background-color: #ffcad4;
+            text-align: center;
         }
+        table {
+        margin: 0 auto;
+        border-collapse: collapse;
+        background-color: #f4acb7;
+    }
     </style>
 </head>
 <body>
-    <table border="1">
+    <table border="5">
         <tr>
-            <th>Nome: </th>
-            <th>Cargo: </th>
+            <th> Nome: </th>
+            <th> Cargo: </th>
+            <th> Salário: </th>
         </tr>
+        <?php 
+            $funcionarios = [
+                ["nome" => "Ana", "cargo" => "Analista", "salario" => 10000],
+                ["nome" => "Carlos", "cargo" => "Desenvolvedor", "salario" => 17000],
+                ["nome" => "Mariana", "cargo" => "Gerente", "salario" => 20000]
+            ];
+
+        // echo "<strong>Funcionário: </strong>" . $funcionarios[2]["nome"] . 
+        // "<strong><br> Cargo: </strong>" . $funcionarios[2]["cargo"] . "<br>";
+
+            foreach ($funcionarios as $f) {
+                echo "<tr> <td>" . $f["nome"] . "</td><td>" . $f["cargo"] . "</td><td>" . $f["salario"] . "</td>";
+            }
+        ?>
     </table>
-    <?php 
-    $funcionarios = [
-        ["nome" => "Ana", "cargo" => "Analista"],
-        ["nome" => "Carlos", "cargo" => "Desenvolvedor"],
-        ["nome" => "Mariana", "cargo" => "Gerente"]
-    ];
-
-    echo "<strong>Funcionário: </strong>" . $funcionarios[2]["nome"] . 
-    "<strong><br> Cargo: </strong>" . $funcionarios[2]["cargo"] . "<br>";
-
-    foreach ($funcionarios as $f) {
-        echo $f["nome"] . " - " . $f["cargo"] . "<br>";
-    }
-?>
 </body>
 </html>

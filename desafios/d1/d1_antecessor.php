7dbd39131862cffadab898c92b840eb18ff4fbc0
Bárbara de Oliveira Cruz <barbara.o.cruz@aluno.senai.br>
2026-05-14 14:06:59 -0300

    14/05

--- desafios/d1/d1_antecessor.php
+++ desafios/d1/d1_antecessor.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Número antecessor e sucessor</title>
+    <title>Número antecessor e sucessor（￣︶￣）↗　</title>
     
     <style>
         body {
